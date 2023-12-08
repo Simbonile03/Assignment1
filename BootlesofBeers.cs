b617052f0f09a55a588b7545c008c59333a95fb7
Simbonile03 <simbonilematshaya03@gmail.com>
2023-12-08 02:48:05 +0200

    mee

---
+++ BootlesofBeers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootlesOfBeers
+{
+    internal class BottlesofBeers
+    {
+
+
+        public int numberOfBeers;
+        public int beersLeft;
+
+
+    }    
+    
+
+}
+        
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BottlesOfBeers 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int beersLeft = 99;
+            while (beersLeft > 2)
+            {
+                Console.WriteLine(beersLeft + " bottles of beer on the wall," + beersLeft + " bottles of beer.");
+                beersLeft--;
+
+                Console.WriteLine("Take one down and pass it around," + beersLeft + " bottles of beer on the wall.");
+            } 
+            Console.WriteLine(beersLeft + " bottles of beer on the wall," + beersLeft + " bottles of beer."); beersLeft--;
+
+
+
+            Console.WriteLine("Take one down and pass it around," + beersLeft + " bottle of beer on the wall.");
+
+
+            Console.WriteLine("1 bottle of beer on the wall, 1 bottle of beer");
+          
+        }
+    }
+}
